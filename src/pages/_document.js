f7be1ff6a10cff981dfd03cdb9d1361d6bdc45ef
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-02-07 19:20:59 +0600

    Improve:  page load performance

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,12 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-      <link href="https://fonts.cdnfonts.com/css/sporting-grotesque" rel="stylesheet" />
-
-      
-
-
-
         </Head>
       <body>
         <Main />
